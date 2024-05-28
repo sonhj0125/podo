b4565a3b72f23068aab16225ee01931cc4ad28e1
rtw1024sj <1024sj@naver.com>
2024-05-28 17:22:58 +0900

    ?
    ?

--- src/main/java/member/model/PointDAO_imple.java
+++ src/main/java/member/model/PointDAO_imple.java
@@ -398,9 +398,6 @@ public class PointDAO_imple implements PointDAO {
 			int currentShowPageNo = Integer.parseInt( paraMap.get("currentShowPageNo") ); 
 			int sizePerPage = Integer.parseInt( paraMap.get("sizePerPage") );
 			
-			System.out.println("currentShowPageNo" + currentShowPageNo);
-			System.out.println("sizePerPage" + sizePerPage);
-			
 			pstmt.setString(1, paraMap.get("userid"));
 			pstmt.setInt(2, (currentShowPageNo * sizePerPage) - (sizePerPage - 1) ); // 공식
 			pstmt.setInt(3, (currentShowPageNo * sizePerPage) ); // 공식
